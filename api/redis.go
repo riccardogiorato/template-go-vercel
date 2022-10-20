a77cc0d59d6a9c98872d8da85ea779a6d3569d6d
Riccardo Giorato <riccardo@teero.com>
2022-10-20 21:38:13 +0200

    added go-redis example

---
+++ api/redis.go
@@ -0,0 +1,44 @@
+package handler
+
+import (
+	"context"
+	"crypto/tls"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/go-redis/redis/v8"
+)
+
+var ctx = context.Background()
+
+var client = redis.NewClient(&redis.Options{
+	Addr:     "global-many-spaniel-31036.upstash.io:31036",
+	Password: os.Getenv("UPSTASH_PASSWORD"),
+	DB:       0,
+	TLSConfig: &tls.Config{
+		MinVersion: tls.VersionTLS12,
+	},
+})
+
+func Redis(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusCreated)
+	w.Header().Set("Content-Type", "application/json")
+
+	// set a foo key on upstash with this value
+	client.Set(ctx, "foo", "value-from-upstash-redis", 0)
+
+	// get the foo key from upstash
+	foo := client.Get(ctx, "foo")
+
+	resp := make(map[string]string)
+	resp["foo"] = foo.Val()
+	body, err := json.Marshal(resp)
+
+	if err != nil {
+		fmt.Printf("Error happened in JSON marshal. Err: %s", err)
+	} else {
+		w.Write(body)
+	}
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module template-go-vercel
 
 go 1.17
+
+require (
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/go-redis/redis/v8 v8.11.5 // indirect
+)
